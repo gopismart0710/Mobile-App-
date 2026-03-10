8e51336448a5128e1311510ebe0d52b78f462813
gopismart0710 <gopi_smart@icloud.com>
2026-03-10 18:19:21 +0530

    Update Dashboard.tsx

--- pages/Dashboard.tsx
+++ pages/Dashboard.tsx
@@ -1,1 +1,19 @@
+import React from "react";
 
+interface Props {
+  lang: string;
+  onNavigate: any;
+  selectedMonth: string;
+  setSelectedMonth: any;
+}
+
+const Dashboard: React.FC<Props> = ({ selectedMonth }) => {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h2>Dashboard</h2>
+      <p>Selected Month: {selectedMonth}</p>
+    </div>
+  );
+};
+
+export default Dashboard;
