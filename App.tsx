af0214a9386e9c3e5168d3421417b44cc4d2ae83
gopismart0710 <gopi_smart@icloud.com>
2026-03-12 18:09:32 +0530

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -47,7 +47,7 @@ const App = () => {
           <div className="flex items-center gap-2">
 
             <img
-              src="/k-logo.png"
+              src="/k logo.png"
               alt="Kanaku"
               className="w-8 h-8 object-contain"
             />
