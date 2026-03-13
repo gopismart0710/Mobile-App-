6377f021e4866d26f5ff50ec6c0a4513895d46a2
gopismart0710 <gopi_smart@icloud.com>
2026-03-13 06:51:00 +0530

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+import SummaryConfirmation from "./pages/SummaryConfirmation";
 import React, { useState, useEffect } from "react";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
