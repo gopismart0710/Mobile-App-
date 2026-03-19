3958052a55522123d9e86c56dfa4b7355b5637b5
gopismart0710 <gopi_smart@icloud.com>
2026-03-19 10:14:48 +0530

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -39,7 +39,7 @@ const App = () => {
 
   return (
 
-    <div className={`min-h-screen bg-slate-50 flex flex-col ${
+<div className={`min-h-screen bg-slate-50 flex flex-col ${
   currentPage === "admin" ? "" : "max-w-md mx-auto"
 }`}>
       {/* Header */}
